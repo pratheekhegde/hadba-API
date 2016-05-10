540a7306a90b17849bbfe0472b4e02f6c1f445a4
Pratheek Hegde <pratheek.hegde@me.com>
2016-05-10 20:09:59 +0530

    [Add] - add rka-ip-address-middleware

--- api/v4/index.php
+++ api/v4/index.php
@@ -16,6 +16,21 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
+
+/*
+* This middleware checks the 'X-Forwarded-For', 'X-Forwarded', 'X-Cluster-Client-Ip', 'Client-Ip' headers for the first IP address it can find.
+* If none of the headers exist, or do not have a valid IP address, then the $_SERVER['REMOTE_ADDR'] is used.
+*
+* Note that the proxy headers are only checked if the first parameter to the constructor is set to true.
+* If set to false, then only $_SERVER['REMOTE_ADDR'] is used
+*/
+
+// Note: Never trust the IP address for security processes!
+$checkProxyHeaders = false;
+$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
 //include database connection
 require_once 'database/connection.php';
 
