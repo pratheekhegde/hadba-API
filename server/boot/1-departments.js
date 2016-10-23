1cb81b31674c03003c84f8b9aeca05505a984bb4
Pratheek Hegde <ptk609@gmail.com>
2016-10-23 17:09:14 +0530

    feat: added department and faculty boot data

---
+++ server/boot/1-departments.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(app) {
+  var Department = app.models.department;
+
+  //MongoDB automigration and collection initialization
+  app.dataSources.hadbaMongo.automigrate('department', function(err) {
+    if (err) throw err;
+
+    var departments = require('./data/departments.json');
+
+    //Creating Facility Data
+    Department.create(departments);
+    console.info('[Boot] - Loaded Department data to DB');
+  });
+};
---
+++ server/boot/2-faculties.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(app) {
+  var Faculty = app.models.faculty;
+
+  //MongoDB automigration and collection initialization
+  app.dataSources.hadbaMongo.automigrate('faculty', function(err) {
+    if (err) throw err;
+
+    var faculties = require('./data/faculties.json');
+
+    //Creating Facility Data
+    Faculty.create(faculties);
+    console.info('[Boot] - Loaded Faculties data to DB');
+  });
+};
