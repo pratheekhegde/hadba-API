0943feb88356b0af35c4fd39d544d4f2875b1ac6
Pratheek Hegde <ptk609@gmail.com>
2016-10-24 20:59:08 +0530

    feat: subjects for cv,ec,me

--- server/boot/03-subjects.js
+++ server/boot/03-subjects.js
@@ -8,9 +8,15 @@ module.exports = function(app) {
     if (err) throw err;
 
     var cs = require('./data/subjects/cs.json');
+    var ec = require('./data/subjects/ec.json');
+    var cv = require('./data/subjects/cv.json');
+    var me = require('./data/subjects/me.json');
 
     //Creating Subject Data
     Subject.create(cs);
+    Subject.create(ec);
+    Subject.create(ec);
+    Subject.create(me);
     console.info('[Boot] - Loaded Subjects data to DB');
   });
 };
