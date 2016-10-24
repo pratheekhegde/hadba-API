00823a2f5f81235d1df97b5c0a737a43d618470e
Pratheek Hegde <ptk609@gmail.com>
2016-10-24 21:12:55 +0530

    feat: added 1st year subjects - Physics cycle and Chemistry cycle. - Made the process of loading async

--- server/boot/03-subjects.js
+++ server/boot/03-subjects.js
@@ -1,22 +1,25 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function(app, cb) {
   var Subject = app.models.subject;
 
   //MongoDB automigration and collection initialization
   app.dataSources.hadbaMongo.automigrate('subject', function(err) {
     if (err) throw err;
 
+    var year1 = require('./data/subjects/1st-year.json');
     var cs = require('./data/subjects/cs.json');
     var ec = require('./data/subjects/ec.json');
     var cv = require('./data/subjects/cv.json');
     var me = require('./data/subjects/me.json');
 
     //Creating Subject Data
+    Subject.create(year1);
     Subject.create(cs);
     Subject.create(ec);
     Subject.create(ec);
     Subject.create(me);
+    process.nextTick(cb);
     console.info('[Boot] - Loaded Subjects data to DB');
   });
 };
